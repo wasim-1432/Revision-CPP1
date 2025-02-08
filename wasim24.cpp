e51dfbe478dc1f6f42c1d285e2b95e9a5c6ebb4e
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-02-08 16:35:31 +0530

    Write a CPP program to calculate the area of the circle and enter the radius by the user.

---
+++ wasim24.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int r;
+    cout<<"Enter the radius of the circle"<<endl;
+    cin>>r;
+    cout<<"Area of the circle="<<3.14*r*r;
+    cout<<endl;
+    return 0;
+}
