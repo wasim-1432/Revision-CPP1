f54fbed573bded541ab3605cc54a8966753188a7
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-02-08 16:42:30 +0530

    Write a CPP to calculate average of three numbers.

---
+++ wasim31.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"Enter any three numbers"<<endl;
+    cin>>a>>b>>c;
+    cout<<"Average of given three numbers="<<(a+b+c)/3.0;
+    cout<<endl;
+    return 0;
+}
