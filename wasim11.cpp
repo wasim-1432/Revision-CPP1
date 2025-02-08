c2b08ee36f0d28ab7a0af8d3e0d101d3868b9b6f
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-02-08 15:49:47 +0530

    Write a CPP program to check whether a given number is Even or Odd number.

---
+++ wasim11.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"Enter any number"<<endl;
+    cin>>a;
+    if(a%2==0)
+    {
+        cout<<"Even number";
+    }
+    else
+    {
+        cout<<"Odd number";
+    }
+    cout<<endl;
+    return 0;
+}
