31b9f6955809d74ab9cac50382494ee47de7658b
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-02-08 16:45:17 +0530

    Write a CPP program to print square of given number.

---
+++ wasim32.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"Enter any number"<<endl;
+    cin>>a;
+    cout<<"Square of given number="<<a*a;
+    cout<<endl;
+    return 0;
+}
